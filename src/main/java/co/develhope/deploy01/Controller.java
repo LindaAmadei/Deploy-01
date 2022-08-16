4214343d9963994f8bd646d7b4967d3e2621a5a2
LindaAmadei <108941806+LindaAmadei@users.noreply.github.com>
2022-08-16 11:56:45 +0200

    esercizio finito deploy 01

---
+++ src/main/java/co/develhope/deploy01/Controller.java
@@ -0,0 +1,13 @@
+package co.develhope.deploy01;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping
+    public String GetdevName(){
+        return "Linda";
+    }
+}
